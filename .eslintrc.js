2da52a91a1b882a6740ca037368c1dc1da0e9c94
DiegoRochaDesenvolvedor <102746033+DiegoRochaDesenvolvedor@users.noreply.github.com>
2024-07-11 11:28:48 -0300

    Create configs and documentations.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'func-style': ['error', 'expression'],
+    'quotes': ['error', 'single'],
+    'space-before-blocks': ['error', 'always'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,17 @@
+# Nome da API
+
+## Introdução
+
+Esta Api baseada em NestJS tem como finalidade simular uma gerenciamento bibliotecario.
+
+## Arquitetura de rotas
+
+![Arquitetura](/docs/api.png 'Arquitetura')
+
+## Endpoints
+
+## Autenticação
+
+- `GET` /endpoint
+
+### Exemplo de Autenticação
